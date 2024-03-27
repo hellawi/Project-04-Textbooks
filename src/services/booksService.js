8a538c8ed8fec4a2a32c868ab5c383378af5bf1c
Michael <55346444+hellawi@users.noreply.github.com>
2024-03-26 20:49:22 -0700

    booksService created

---
+++ src/services/booksService.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export function getAllBooksService(params) {
+  return axios.get('/books', {
+    params,
+  });
+}
+
+export function createBookService(data) {
+  return axios.post('/books', data);
+}
+
+export function findBookById(bookId) {
+  return axios.post(`/books/${bookId}`);
+}
+
+export function updateBookService(bookId, data) {
+  return axios.put(`/books/${bookId}`, data);
+}
+
+export function deleteBookService(bookId, data) {
+  return axios.delete(`/books/${bookId}`, data);
+}
